8d2fd77d90cd384daa5c261436abbefba4b65863
Egor <egor.rozinskiy@mail.ru>
2015-10-02 17:54:48 +0400

    f

---
+++ upr10.py
@@ -0,0 +1,11 @@
+from random import random
+
+inp = str(input())
+x = list(map(int,inp.split(" ")))
+y = int(input())
+for i in range(y):
+    x.insert(len(x)-1-x[len(x)- 1],x[len(x)-1])
+    x.reverse()
+    x.remove(x[0])
+    x.reverse()
+print(x)
