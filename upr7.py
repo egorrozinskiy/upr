63fd114ac223dc9c111f646f86eb4dbbd1ac636e
Egor <egor.rozinskiy@mail.ru>
2015-10-02 17:46:10 +0400

    f
    f

--- upr7.py
+++ upr7.py
@@ -1,17 +1,11 @@
-input = open('int_data.txt', 'r')
-A = input.readlines()
-for i in range(len(A)):
-	A[i] = int(A[i])
-k = 0
-lx = ln = A[0]
-for i in range(len(A)):
-	if A[0:i].count(A[i]) == 0:
-		print(A[i], A.count(A[i]), 'r', k)
-		k += 1
-	x = A.count(A[i])	
-	if A.count(ln) > x:
-		ln = A[i]
-	if x > A.count(lx):
-		lx = A[i]
-print(lx, ln)
-print(k)
+inp = open("int_data.txt")
+file = str(inp.read())
+file = file[:-1]
+counts = []
+ch = list(map(int,file.split(" ")))
+for i in range(max(ch)+1):
+    counts.append(ch.count(ch[i]))
+print('kol-vo ',i,'?????:',ch.count(ch[i]))
+print ('chasto:',counts.index(max(counts)))
+print ('redko:',counts.index(min(counts)))
+print ('razlichnie:',(101-counts.count(0)))
